fc2e5607d711f852a2ed3f75a4ed94d368f601a1
asrodeno18 <47376395+asrodeno18@users.noreply.github.com>
2020-10-11 01:51:36 -0500

    Update Rscript.R

--- Rscript.R
+++ Rscript.R
@@ -47,7 +47,7 @@ library(dplyr)
 require(plyr)
 
 importbcrp<-function(x,y,z){
- for (i in x) {
+  
   # Indicamos el ID, el formato, las fechas y el idioma. Llamamos "url" a esta combinación
   url <-paste('https://estadisticas.bcrp.gob.pe/estadisticas/series/api/',x,'/json/',y,'/',z,'/', sep="") 
   # Descargamos el url indicado
@@ -56,41 +56,15 @@ importbcrp<-function(x,y,z){
   dato <-as.data.frame(lapply(tmp1$periods, function(y) gsub("n.d.", "-99999.99", y))) 
   # Cambiamos el nombre del archivo usando el ID que lo identifica.
   names(dato) <- c("Fecha",paste("",x,"",sep=""))
-  View(dato)
-  write.table(dato, file=paste(x,'.dta', sep=""))
- }
+  dato
+  write.table(dato, "misdatos.txt", sep = "\t", quote = F, row.names = F)
+ 
 }
 
 
 importbcrp('PN01205PM','2005-8','2020-9')
 
 
-
-
-
-
-
-
-
-metadato <- c('PN03371FQ','PN03372FQ','PN03373FQ')
-
-for (i in 1:length(metadato)){
-  #  tryCatch({
-  # Indicamos el ID, el formato, las fechas y el idioma. Llamamos "url" a esta combinación
-  url <-paste('https://estadisticas.bcrp.gob.pe/estadisticas/series/api/',metadato[i],'/json/1940/2018/ing', sep="") 
-  # Descargamos el url indicado
-  tmp1  <- fromJSON(readLines(url, warn="F"))
-  # Cambiamos el formato de los datos y cambiamos el valor de las variables con missing values.
-  dato <-as.data.frame(lapply(tmp1$periods, function(y) gsub("n.d.", "-99999.99", y))) 
-  # Cambiamos el nombre del archivo usando el ID que lo identifica.
-  names(dato) <- c("name",paste("",metadato[i],"",sep=""))
-  # Creamos un nuevo documento indicando el formato
-  # El formato "dta" se usa en el software Stata
-  write.dta(dato, file=paste(metadato[i],'.dta', sep=""))
-  # El formato "csv" se puede usar en distintos softwares incluído Excel
-  write.csv2(dato, file=paste(metadato[i],'.csv', sep=""))
-  #  }, error=function(e){})
-}
 
 
 
